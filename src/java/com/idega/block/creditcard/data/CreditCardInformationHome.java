141ef11bcdd97d175b1e791fde5cdde8d5ad684b
Thomas Hilbig <thomas@idega.com>
2004-07-14 15:53:51 +0000

    CreditCardInformation was moved from block credit card to block trade avoiding that both blocks depends on each other.

--- src/java/com/idega/block/creditcard/data/CreditCardInformationHome.java
+++
@@ -1,8 +0,0 @@
-package com.idega.block.creditcard.data;
-
-public interface CreditCardInformationHome extends com.idega.data.IDOHome
-{
- public CreditCardInformation create() throws javax.ejb.CreateException;
- public CreditCardInformation findByPrimaryKey(Object pk) throws javax.ejb.FinderException;
-
-}
