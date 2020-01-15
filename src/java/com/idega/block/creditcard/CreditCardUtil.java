aef30b748bf32e86fbdbf2f6e2e6c326e684de39
Valdas Žemaitis <valdas@idega.com>
2020-01-15 12:29:13 +0200

    New method

--- src/java/com/idega/block/creditcard/CreditCardUtil.java
+++ src/java/com/idega/block/creditcard/CreditCardUtil.java
@@ -1,5 +1,6 @@
 package com.idega.block.creditcard;
 
+import com.idega.util.CoreConstants;
 import com.idega.util.StringUtil;
 
 public class CreditCardUtil {
@@ -10,6 +11,19 @@ public class CreditCardUtil {
 		}
 
 		return creditCardNumber.length() <= 4 ? creditCardNumber : "****-****-****-".concat(creditCardNumber.substring(creditCardNumber.length() - 4));
+	}
+
+	public static final String getMaskedSecurityCode(String securityCode) {
+		if (StringUtil.isEmpty(securityCode)) {
+			return null;
+		}
+
+		String masked = CoreConstants.EMPTY;
+		for (int i = 0; i < securityCode.length(); i++) {
+			masked = masked.concat(CoreConstants.STAR);
+		}
+
+		return masked;
 	}
 
 }
