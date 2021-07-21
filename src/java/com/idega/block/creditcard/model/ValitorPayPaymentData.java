dc8021c481c76af65dcaba114124c9e5171a029c
Valdas Žemaitis <valdas@idega.com>
2021-07-21 10:24:26 +0300

    Removed needless code

--- src/java/com/idega/block/creditcard/model/ValitorPayPaymentData.java
+++ src/java/com/idega/block/creditcard/model/ValitorPayPaymentData.java
@@ -65,7 +65,6 @@ public class ValitorPayPaymentData implements Serializable {
 		this.cvc = cvc;
 		this.cardholderDeviceType = cardholderDeviceType;
 		this.virtualCardData = virtualCardData;
-		//this.displayName = displayName;
 	}
 
 	public ValitorPayPaymentData(
