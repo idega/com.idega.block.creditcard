351228122506626faa6411622280abd1892861bc
Valdas Žemaitis <valdas@idega.com>
2023-06-01 10:38:53 +0300

    New constants

--- src/java/com/idega/block/creditcard/CreditCardConstants.java
+++ src/java/com/idega/block/creditcard/CreditCardConstants.java
@@ -2,25 +2,32 @@ package com.idega.block.creditcard;
 
 public interface CreditCardConstants {
 
-	public static final String CREDIT_CARD_MERCHANT_ID = "CC_MERCHANT_ID";
+	public static final String	CREDIT_CARD_MERCHANT_ID = "CC_MERCHANT_ID",
 
-	public static final String REDIRECTION_URL = "REDIRECTION_URL";
+								REDIRECTION_URL = "REDIRECTION_URL",
 
-	public static final String SUCCESS = "SUCCESS";
-	public static final String METADATA_SUCCESS = SUCCESS;
+								SUCCESS = "SUCCESS",
+								METADATA_SUCCESS = SUCCESS,
+								PAYMENT_COMPLETED = "PAYMENT_COMPLETED",
+								PAYMENT_SUCCEEDED = "PAYMENT_SUCCEEDED",
 
-	public static final String CARD_HOLDER_DEVICE_TYPE_WWW = "WWW";
+								CARD = "card",
+								CLOSED = "CLO",
 
-	public static final String OPERATION_SALE = "Sale";
-	public static final String TRANSACTION_TYPE_ECOMMERCE = "ECommerce";
+								CARD_HOLDER_DEVICE_TYPE_WWW = "WWW",
 
-	public static final String FULL_DATE_TIME_NO_DELIMITERS_STRING = "yyyyMMddHHmmssSSSS";
+								OPERATION_SALE = "Sale",
+								TRANSACTION_TYPE_ECOMMERCE = "ECommerce",
 
-	public static final String METADATA_CARD_VERIFICATION_MDSTATUS = "CARD_VERIFICATION_MDSTATUS";
-	public static final String METADATA_CARD_VERIFICATION_CAVV = "METADATA_CARD_VERIFICATION_CAVV";
-	public static final String METADATA_CARD_XID = "METADATA_CARD_XID";
+								FULL_DATE_TIME_NO_DELIMITERS_STRING = "yyyyMMddHHmmssSSSS",
 
-	public static final String SUBSEQUENT_TRANSACTION_TYPE_RECURRING = "MerchantInitiatedRecurring";
-	public static final String INITIATION_REASON_RECURRING = "Recurring";
+								METADATA_CARD_VERIFICATION_MDSTATUS = "CARD_VERIFICATION_MDSTATUS",
+								METADATA_CARD_VERIFICATION_CAVV = "METADATA_CARD_VERIFICATION_CAVV",
+								METADATA_CARD_XID = "METADATA_CARD_XID",
+
+								SUBSEQUENT_TRANSACTION_TYPE_RECURRING = "MerchantInitiatedRecurring",
+								INITIATION_REASON_RECURRING = "Recurring",
+
+								QUALIFIER_FINANCE_SERVICE = "ccFinanceService";
 
 }
